54e92321a8bfeabb3ae61442c649d481c438bdd9
Victor Da Costa Vasco <victordcvasco@gmail.com>
2025-11-26 21:10:38 -0300

    Criei Record - AlunoDTO e ProfessorDTO

---
+++ src/main/java/com/agendamentos/dto/AlunoDTO.java
@@ -0,0 +1,8 @@
+package com.agendamentos.dto;
+
+public record AlunoDTO(
+		Long id,
+		String name,
+		String email,
+		String phone
+		) {}
---
+++ src/main/java/com/agendamentos/dto/ProfessorDTO.java
@@ -0,0 +1,8 @@
+package com.agendamentos.dto;
+
+public record ProfessorDTO(
+		Long id,
+		String name,
+		String email,
+		String phone
+		) {}
